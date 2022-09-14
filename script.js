6934488c0390c8b76548872e9cbfce7008a80eba
Giovana Paludo <giovanacpaludo@gmail.com>
2022-09-14 16:14:05 -0300

    Adiciona requisitos 1, 2, 4 e parte do 3

---
+++ script.js
@@ -0,0 +1,16 @@
+const submitBtn = document.querySelector('#sumbit-btn');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+// function validation() {
+//     const trybeEmail = "tryber@teste.com";
+//     const trybePassword = "123456";
+//         if (email.value !== trybeEmail || password.value !== trybePassword) {
+//             alert("Email ou senha inválidos.");
+//         }
+//         else {
+//             alert("Olá, Tryber!");
+//         }
+// }
+
+// submitBtn.addEventListener('click', validation());
