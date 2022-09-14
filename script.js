ad5a579fbfc2ccde3d901b94dcec0a1b2fac0acd
Giovana Paludo <giovanacpaludo@gmail.com>
2022-09-14 18:18:09 -0300

    Adiciona requisitos 3, 5, 6, 8 e 9

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const submitBtn = document.querySelector('#sumbit-btn');
+const submitBtn = document.querySelector('#submit-btn');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
-// function validation() {
-//     const trybeEmail = "tryber@teste.com";
-//     const trybePassword = "123456";
-//         if (email.value !== trybeEmail || password.value !== trybePassword) {
-//             alert("Email ou senha inválidos.");
-//         }
-//         else {
-//             alert("Olá, Tryber!");
-//         }
-// }
+function validation() {
+  const trybeEmail = 'tryber@teste.com';
+  const trybePassword = '123456';
+  if (email.value !== trybeEmail || password.value !== trybePassword) {
+    alert('Email ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
 
-// submitBtn.addEventListener('click', validation());
+submitBtn.addEventListener('click', validation);
