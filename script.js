0592836cf8fd46ebcd553399836faa535af6b9c9
Giovana Paludo <giovanacpaludo@gmail.com>
2022-09-15 19:29:03 -0300

    Adiciona requisitos 17, 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const submitBtn = document.querySelector('#submit-btn');
+const submitBtn1 = document.querySelector('#submit-btn1');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const submitBtn2 = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
+
+submitBtn2.disabled = true;
 
 function validation() {
   const trybeEmail = 'tryber@teste.com';
@@ -12,4 +16,13 @@ function validation() {
   }
 }
 
-submitBtn.addEventListener('click', validation);
+function agreementValidation() {
+  if (agreement.checked) {
+    submitBtn2.disabled = false;
+  } else {
+    submitBtn2.disabled = true;
+  }
+}
+
+submitBtn1.addEventListener('click', validation);
+agreement.addEventListener('click', agreementValidation);
